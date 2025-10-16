09e8bf3ec3f905306fb0db7a5a32fa316a66a0db
Caique Augusto <caique.augusto@unsoft.com.br>
2025-10-16 15:46:51 -0300

    nova tela com constraint layout

--- tela9/app/src/main/java/com/pdm/tela9/MainActivity.kt
+++ tela9/app/src/main/java/com/pdm/tela9/MainActivity.kt
@@ -17,33 +17,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-
-        val emailInput = findViewById<EditText>(R.id.editEmail)
-        val passwordInput = findViewById<EditText>(R.id.editPassword)
-        val loginButton = findViewById<Button>(R.id.btnLogin)
-
-        // simulação botão de login.
-        loginButton.setOnClickListener {
-            val email = emailInput.text.toString().trim()
-            val password = passwordInput.text.toString().trim()
-
-            //Verificar se algum dos campos está vazio.
-            if (email.isBlank() || password.isBlank()) {
-                Toast.makeText(this, "Por favor, preencha todos os campos.", Toast.LENGTH_SHORT).show()
-            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                Toast.makeText(this, "Por favor, insira um e-mail válido.", Toast.LENGTH_SHORT).show()
-            }else if (passwordInput.length()<8){
-                Toast.makeText(this,"A senha deve possuir 8 caracteres",Toast.LENGTH_SHORT).show()
-            }else {
-                Toast.makeText(this, "Login efetuado com sucesso!", Toast.LENGTH_SHORT).show()
-                // lógica de login real veria aqui
-            }
-        }
     }
 }
